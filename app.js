dd097a69bf243772c2642dbecfb4118f1ad14c45
Thor Löfgren <lofgrenthor@gmail.com>
2023-01-23 21:01:32 +0100

    feature(employee page): Inital html and css
    Added bootstrap to the project to have a button that collapses/reveals
    a form to submit new purchase requests.

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use("/", homeRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/", authRoutes);
 
+const usersRoutes = require("./routes/users.routes");
+app.use("/", usersRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
