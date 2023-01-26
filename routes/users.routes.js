283b5ab56d71f52d34fd4025853a39c8a91a655e
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-01-26 21:32:54 +0100

    refactor logic to controllers

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -8,6 +8,8 @@ const {
   getUserPage,
   postNewPurchase,
   postProcessPurchaseRequest,
+  getAdminPage,
+  postAdminPage,
 } = require("../controllers/users.controllers");
 
 router.get("/user", isLoggedIn, isNotAdmin, getUserPage);
@@ -16,39 +18,9 @@ router.post("/user/newPurchase", postNewPurchase);
 
 router.post("/user/proccesPurchaseRequest", postProcessPurchaseRequest);
 
-router.get("/admin", isLoggedIn, isAdmin, async (req, res, next) => {
-  try {
-    const user = req.session.currentUser;
-    const company = await Company.findById(user.company).populate('users');
+router.get("/admin", isLoggedIn, isAdmin, getAdminPage);
 
-    const usersList = company.users
-
-    usersList.sort((first, second) => {
-      if (first.status === "Pending") return -1;
-      else return 1;
-    });
-
-    res.render("users/admin", { style: "users/admin.css", user, usersList });
-  } catch (error) {
-    next(error)
-  }
-});
-
-router.post('/user/edit', async (req, res, next) => {
-  const { status, role, id } = req.body
-  
-  try {
-    const user = await User.findById(id)
-
-    if (status) user.status = status;
-    user.role = role;
-    await user.save()
-    
-    res.redirect('/admin')
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/user/edit', isLoggedIn, isAdmin, postAdminPage)
 
 
 module.exports = router;
