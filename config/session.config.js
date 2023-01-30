7fa4d8c5976ca44e1c5b60f75f56a0435ee6e4f0
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-01-30 21:57:18 +0100

    session config for deployment

--- config/session.config.js
+++ config/session.config.js
@@ -1,19 +1,22 @@
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 
-module.exports = app => app.use(
-    session({
-        secret: process.env.SESS_SECRET,
-        resave: true,
-        saveUninitialized: false,
-        cookie: {
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            secure: process.env.NODE_ENV === 'production',
-            httpOnly: true,
-            maxAge: 36000000,
-        },
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/erp-costmanager"
+module.exports = app => {
+    app.set('trust proxy', 1);
+    app.use(
+        session({
+            secret: process.env.SESS_SECRET,
+            resave: true,
+            saveUninitialized: false,
+            cookie: {
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+                secure: process.env.NODE_ENV === 'production',
+                httpOnly: true,
+                maxAge: 36000000,
+            },
+            store: MongoStore.create({
+                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/erp-costmanager"
+            })
         })
-    })
-)
+    )
+}
